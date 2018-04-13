96e5f058e5bc5f1d1859d8ae1e358bff830e4bd7
Jiayi Shi <jsfkc@mail.missouri.edu>
2018-04-13 15:33:37 -0500

    java

--- menu.js
+++ menu.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
     $('nav ul').slideToggle(500);
   });//end slide toggle
 
+
+
+
 //When window is resized, the display:none is turned off for the ul so the menu displays on click or touch.
   $(window).resize(function() {
 	   if (  $(window).width() > 700 ) {
